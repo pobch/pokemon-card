0418555fea32435dec8524687f713f16d375938f
Pob <590650@gmail.com>
2021-03-09 00:12:21 +0700

    useQueries

--- src/OriginalPokemon.tsx
+++ src/OriginalPokemon.tsx
@@ -1,5 +1,6 @@
 import { Typography } from 'antd'
-import { useQuery } from 'react-query'
+import { useQuery, useQueries } from 'react-query'
+import { useLocation } from 'react-router-dom'
 import axios from 'axios'
 
 const URL = 'https://pokeapi.co/api/v2/pokemon?limit=30'
@@ -7,12 +8,42 @@ const URL = 'https://pokeapi.co/api/v2/pokemon?limit=30'
 const { Title } = Typography
 
 function OriginalPokemon() {
-  const { isLoading, error, data, isFetching } = useQuery('pokemons', async () => {
-    const response = await axios.get(URL)
-    return response.data
-  })
+  const querystring = new URLSearchParams(useLocation().search)
+  const page = Number(querystring.get('page'))
+  let offset = 0
+  if (page && page >= 1) {
+    offset = (page - 1) * 30
+  }
 
-  console.log('DATA: ', data)
+  const {
+    status: pokemonsStatue,
+    error: pokemonsError,
+    data: pokemons,
+    isFetching: pokemonsIsFetching,
+    isPreviousData: pokemonsIsPreviousData,
+  } = useQuery(
+    ['pokemons', offset],
+    async () => {
+      const response = await axios.get(URL + '&offset=' + offset)
+      return response.data
+    },
+    { keepPreviousData: true }
+  )
+
+  const pokemonUrls =
+    pokemons?.results?.map((pokemon: { name: string; url: string }) => pokemon.url) ?? []
+
+  const pokemonQueries = useQueries(
+    pokemonUrls.map((url: string) => {
+      return {
+        queryKey: ['pokemon', url],
+        queryFn: async () => {
+          const response = await axios.get(url)
+          return response.data
+        },
+      }
+    })
+  )
 
   return (
     <>
